34712836e6de12d1e64bea00d52c01fb026b1ea5
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-06-20 20:34:10 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 su rhel -c 'ssh-keygen -f /home/rhel/.ssh/id_rsa -q -N ""'
-
+echo "10.0.2.2 control.lab control" >> /etc/hosts
 # # ## setup rhel user
 # touch /etc/sudoers.d/rhel_sudoers
 # echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
