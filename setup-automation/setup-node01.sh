9616b9aa53b57037e02dd366ff10e2a9b5a135ab
Nuno Martins <nmartins@redhat.com>
2025-06-22 15:33:09 +0200

    Update setup-node01.sh

--- setup-automation/setup-node01.sh
+++ setup-automation/setup-node01.sh
@@ -4,7 +4,43 @@ update-ca-trust
 rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 
 subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
-dnf install samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation -y
+dnf install samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation httpd -y
 setenforce 0
 
 cp /etc/resolv.conf /tmp/resolv.conf
+
+
+cat <<EOF | tee /var/www/html/index.html
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nothing to See Here</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            color: #333;
+        }
+        h1 {
+            font-size: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Nothing to See Here - Not Yet Anyway - Node01 </h1>
+</body>
+</html>
+
+EOF
+
+systemctl start httpd
