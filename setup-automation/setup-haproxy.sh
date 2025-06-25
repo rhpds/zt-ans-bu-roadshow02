bca6b1cc644d3a3b8e1f491ef24435cdf2a49628
Nuno Martins <nmartins@redhat.com>
2025-06-25 16:27:27 +0200

    Update setup-haproxy.sh

--- setup-automation/setup-haproxy.sh
+++ setup-automation/setup-haproxy.sh
@@ -1,3 +1,12 @@
+curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+update-ca-trust
+rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
+
+subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
+dnf install samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation httpd -y
+setenforce 0
+
+
 sudo dnf update
 sudo dnf install haproxy nano -y
 setenforce 0
