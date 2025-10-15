e97e0b127b86a01a2377ccd1ea41349f4bc43d0c
Nuno Martins <nmartins@redhat.com>
2025-10-15 22:15:16 +0200

    Update setup-node01.sh

--- setup-automation/setup-node01.sh
+++ setup-automation/setup-node01.sh
@@ -11,12 +11,23 @@ echo "192.168.1.100 windows.lab windows" >> /etc/hosts
 echo "192.168.1.101 dbserver.lab dbserver" >> /etc/hosts
 
 
-curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
-update-ca-trust
-rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
 
-subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
-dnf install samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation httpd -y
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+retry "rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
+retry "install samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation httpd git -y"
 setenforce 0
 
 cp /etc/resolv.conf /tmp/resolv.conf
